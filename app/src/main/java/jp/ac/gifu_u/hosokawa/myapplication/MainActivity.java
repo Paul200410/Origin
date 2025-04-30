aafa0994781fd46ebf037bd80a5ebb5550de8789
Paul200410 <kyouben594@gmail.com>
2025-04-30 23:12:06 +0900

    4/30

--- app/src/main/java/jp/ac/gifu_u/hosokawa/myapplication/MainActivity.java
+++ app/src/main/java/jp/ac/gifu_u/hosokawa/myapplication/MainActivity.java
@@ -1,12 +1,16 @@
 package jp.ac.gifu_u.hosokawa.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsCompat.Type;
+import androidx.core.view.WindowInsetsCompat.Insets;
+import androidx.core.view.EdgeToEdge;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +19,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // EdgeToEdgeの設定（既存）
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            Insets systemBars = insets.getInsets(Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // 🔽 ここから追記：ボタンの処理を実装
+        Button exitButton = findViewById(R.id.exitButton);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "アプリを終了しました", Toast.LENGTH_SHORT).show();
+                finish();
+            }
         });
     }
 }
