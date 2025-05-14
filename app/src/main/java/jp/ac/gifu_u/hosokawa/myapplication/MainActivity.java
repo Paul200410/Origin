552155b9befd8d3408a3b0fcf573d35d228d4d6f
Paul200410 <kyouben594@gmail.com>
2025-05-14 16:26:40 +0900

    5/14

--- app/src/main/java/jp/ac/gifu_u/hosokawa/myapplication/MainActivity.java
+++ app/src/main/java/jp/ac/gifu_u/hosokawa/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.Locale;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -58,9 +59,34 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         double lat = location.getLatitude();
         double lng = location.getLongitude();
 
-        String text = String.format("緯度: %.5f\n経度: %.5f", lat, lng);
-        locationText.setText(text);
+        String locationStr = String.format(Locale.US, "緯度: %.3f 経度: %.3f", lat, lng);
 
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, locationStr, Toast.LENGTH_SHORT).show();
+        locationText.setText(locationStr);  // TextView に表示
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 100) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 権限が許可されたので再度リスナー登録
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            1000,
+                            10,
+                            this
+                    );
+                }
+            } else {
+                Toast.makeText(this, "位置情報の権限が必要です", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+
 }
